3f320ada6699672abbfcfbf4749e1ed75f1ad33f
CrimsonStorm <jlshaw@ucdavis.edu>
2018-08-16 08:11:29 -0700

    Adding Static Webpage
    Static webpage has one video built in with a button to take hidden status away.

---
+++ Website_Demo/Click.js
@@ -0,0 +1,7 @@
+// Note that for a server this function will have a GET request to the DB in order to retreive the mp4 instead
+// of the Video being hard coded in. It isn't that much different.
+
+function showVideo() {
+	
+		document.getElementById("HUC").style.display = "flex";
+}
